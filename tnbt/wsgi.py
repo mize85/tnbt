e171a79807964db80f2e30ec2b8c53443845af31
mize85 <Mize85@gmx.net>
2015-05-09 17:03:44 +0200

    wsgi amazon settings

--- tnbt/wsgi.py
+++ tnbt/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tnbt.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tnbt.settings.amazon")
 
 application = get_wsgi_application()
