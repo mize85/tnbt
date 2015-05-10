1ab6d6cca24d66e2c9adf2236224ae647a021b29
mize85 <Mize85@gmx.net>
2015-05-10 15:51:35 +0200

    python config

--- frontend/web-app/config/environment.js
+++ frontend/web-app/config/environment.js
@@ -47,10 +47,10 @@ module.exports = function(environment) {
     ENV.contentSecurityPolicy = {
       'default-src': "'none'",
       'script-src': "'self'", // Allow scripts from https://cdn.mxpnl.com
-      'font-src': "'self' ", // Allow fonts to be loaded from http://fonts.gstatic.com
+      'font-src': "'self' https://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
       'connect-src': "'self' "+HOST, // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
       'img-src': "'self' ",
-      'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
+      'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
       'media-src': "'self'"
     };
 
